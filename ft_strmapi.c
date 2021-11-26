6ccfee4763576be29192d4d198383ee378274fed
p0tt3r-iOS <france998811@gmail.com>
2021-11-26 16:12:12 +0900

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: dha <dha@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 23:12:42 by dha               #+#    #+#             */
-/*   Updated: 2021/11/25 16:46:56 by dha              ###   ########.fr       */
+/*   Updated: 2021/11/25 20:22:16 by dha              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 {
-
+	char			*ret;
+	unsigned int	i;
+	unsigned int	len;
+	
+	i = 0;
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	ret = (char *) malloc(sizeof(char) * (len + 1));
+	if (ret == 0)
+		return (0);
+	while (i < len)
+		ret[i++] = f(i, s[i]);
+	return (ret);
 }
